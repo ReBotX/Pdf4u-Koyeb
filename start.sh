fbdeb7c2c0083661d43372122c2e32759ae9d792
ReBotX <newrebot2@outlook.com>
2025-04-06 12:45:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ then
   git clone https://github.com/Pdf4u/HerokuPDF4U.git /HerokuPDF4U
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /HerokuPDF4U
+  git clone $UPSTREAM_REPO /Pdf4u-Koyeb
 fi
 cd /HerokuPDF4U
 pip3 install -U -r requirements.txt
